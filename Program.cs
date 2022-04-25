6345a45c7f8226fac11a7dee7629531db1792015
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-04-25 15:14:18 -0300

    add construtor em Arus

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using static System.Console;
+
+using p03_jogoRPG.src.Entities;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Arus heroA = new Arus("brecarv", 25, "Raider");
+
+    WriteLine($"O héroi {heroA.Name} é da classe {heroA.HeroType} e está no nível {heroA.Level}.");
+  }
+}
--- src/Entities/Arus.cs
+++ src/Entities/Arus.cs
@@ -2,6 +2,12 @@ namespace p03_jogoRPG.src.Entities
 {
   public class Arus
   {
+    public Arus(string Name, int Level, string HeroType)
+    {
+      this.Name = Name;
+      this.Level = Level;
+      this.HeroType = HeroType;
+    }
     public string Name;
     public int Level;
     public string HeroType;
